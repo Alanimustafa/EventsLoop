460edf308dc719046c9918b26eda1a979b35b913
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-16 19:43:43 -0600

    308A-1 LAB - Part 3 equation correction

--- app.js
+++ app.js
@@ -48,5 +48,32 @@ trampoline(theArray);
 
 
 
+// Part 3: Deferred Execution
+
+// The function to find the prime number 
+function findPrimeNumbers (startNumber, endNumber) {
+  let allPrimeNumbers = [];
+    if (startNumber >=3) {
+      for (let i = startNumber; i <= Math.sqrt(startNumber) && i <= endNumber ; i++) { 
+        if (startNumber % i === 0) { return false}
+      allPrimeNumbers.push(startNumber);  
+    } 
+    
+     allPrimeNumbers;
+
+    }
+
+    console.log (`All prime numbers between ${startNumber} and ${endNumber} are `,allPrimeNumbers);
+}
+
+
+
+findPrimeNumbers(1,100);
+const htmlElement = document.createElement('div');
+htmlElement.textContent = ""
+
+
+
+
 
 
